76fda4ff8514a991dcd486218ff06432a5556efd
hoss <agcacho22@gmail.com>
2021-04-13 22:14:05 -0700

    added wallet utils

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="react-scripts" />
+interface WindowChain {
+  ethereum?: {
+    isMetaMask?: true;
+    request: (...args: any[]) => void;
+  };
+}
+
+// Target the module containing the `ProcessEnv` interface
+// https://www.typescriptlang.org/docs/handbook/declaration-merging.html#module-augmentation
+declare namespace NodeJS {
+  // Merge the existing `ProcessEnv` definition with ours
+  // https://www.typescriptlang.org/docs/handbook/declaration-merging.html#merging-interfaces
+  export interface ProcessEnv {
+    NODE_ENV: "development" | "production" | "test";
+    REACT_APP_CHAIN_ID: string;
+  }
+}
