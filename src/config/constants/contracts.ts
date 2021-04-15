1025901de0ae173c0416b197aa10b40bbb5cb9dd
CJ <wynautcommunitywebdev@gmail.com>
2021-04-15 18:35:01 -0500

    Update contracts.ts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -4,56 +4,45 @@
 
 const contracts = {
   // Infrastructure
-  // Unova
   unova: {
     97: "0x700b57582d08620966adb67396D6723Fa38b0370",
     56: "0x283e3AB66889E49dAFac461eC983E81B49Fb3Cb3",
   },
-  // Wynaut Bridge
   wynautBridge: {
     97: "0x342292935A16d9655D54663083763A6012CF945a",
     56: "0x1b830890347768297100DD5dc4f38af34aAF38b8",
   },
-  // Meowth Bridge
   meowthBridge: {
     97: "0xd7aC26A8125E0AD80049AD59f72d7ffE67D4117b",
     56: "0xcf39DEC43eAB07D70C3D32776F3ae255ADF69f22",
   },
-  // BrindletonBay (PB-2114 Bank)
+  // Banks
   brindletonBay: {
     56: "0xa3448A6D8862cc5F481655D65fcAAAD206BD5964",
   },
-  // BNB-BUSD
+  // PokeMoon LP Tokens
   bnbBusd: {
     56: "0x7b9c5504A3ae1DD3649e97D0Ff36aC80Ec7fb409",
   },
-  // MNT-BNB
   mntBnb: {
     56: "0x85d1C8091CCD6aC92774C4FF38b82B4df42c6662",
   },
-  // MNT-BUSD
   mntBusd: {
     56: "0x5726887Ee7648968996Cc6dEbF997D5c831BcDa7",
   },
-  // KBN-BNB
   kbnBnb: {
     56: "0x290836C9e6bDC2b89a26AD91B039564F5fa3A731",
   },
-  // KBN-BUSD
   kbnBusd: {
     56: "0xE81BdBD70DE7d324f23525d3f97c11F028D237b8",
   },
-  // PB-BNB
   pb2114Bnb: {
     56: "0xf65bB897fdCc3e734957BA479EA90F3559b838C2",
   },
-  // Final Tokens
-  // Meownaut
   meownaut: {
     97: "0xa96D0F886d9017011DB9405946dDc8ce8F8D244B",
     56: "0x498e3739d58AAe82656030BCBcCf5ac63E0E57e1",
   },
-  // Koban
   koban: {
     56: "0x030708208dC29B1688b212081F31cDB59097A67D",
   },
@@ -61,22 +50,21 @@ const contracts = {
   pb2114: {
     56: "0xBEEa03a1768b8CFa0496B4e3aCf3bf5Cc7d69904",
   },
-  // Meowth
+  // Old Tokens
   meowth: {
     97: "0x6310b990B73a284cC212e8c113CD9868A2CCaE5C",
     56: "0xe561479bebee0e606c19bb1973fc4761613e3c42",
   },
-  // Wyanut
   wynaut: {
     97: "0xabEA66590051496808C79317201E5C9d8Ab517F9",
     56: "0x067a5ad3f0f91acf512ffe66ea77f37b4dcaaf18",
   },
-  // OTHER TOKENS
+  // Other Tokens
   cake: {
     97: "0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe",
     56: "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82",
   },
-  // Pegs
+  // Binance Pegs
   wbnb: {
     97: "0x4921a7ba7e795ecaf4da59da536a5563e88cea0f",
     56: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
