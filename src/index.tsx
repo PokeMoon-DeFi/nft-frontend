e99e6fc689e647bdf0d9e60d6574f7ff0d3560ce
CJ <wynautcommunitywebdev@gmail.com>
2021-04-15 18:43:00 -0500

    Add Landing Page, remove webvitals

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { Web3ReactProvider } from "@web3-react/core";
 import { getLibrary } from "utils/web3React";
 import { Provider } from "react-redux";
@@ -18,8 +17,3 @@ ReactDOM.render(
   </Web3ReactProvider>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
