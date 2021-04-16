fcd1947be0bb53bee467b238d60a5b722b1ee32e
hoss <agcacho22@gmail.com>
2021-04-15 22:24:32 -0700

    broken allowance

--- src/utils/contractHelpers.ts
+++ src/utils/contractHelpers.ts
@@ -2,6 +2,7 @@ import Web3 from "web3";
 import { AbiItem } from "web3-utils";
 import web3NoAccount from "./web3";
 import bep20Abi from "config/constants/abi/bep20.json";
+import pokemoonNft from "config/constants/abi/pokemoonnft.json";
 import contracts from "config/constants/contracts";
 
 const getContract = (abi: any, address: string, web3?: Web3) => {
@@ -9,8 +10,21 @@ const getContract = (abi: any, address: string, web3?: Web3) => {
   return new _web3.eth.Contract((abi as unknown) as AbiItem, address);
 };
 
+export const getAddress = (name: string) => {
+  return contracts[name][process.env.REACT_APP_CHAIN_ID];
+};
+
+export const getNftContract = () => {
+  return getContract(pokemoonNft, getAddress("pokemoonNft"));
+};
+
 export const getBep20Contract = (address: string, web3?: Web3) => {
   return getContract(bep20Abi, address, web3);
+};
+
+export const getContractFromSymbol = (symbol: string) => {
+  const address = getAddressFromSymbol(symbol);
+  return getBep20Contract(address);
 };
 
 export const getAddressFromSymbol = (symbol: string) => {
