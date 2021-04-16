efa75fcd9b482a91d8e1f88663a1c1d63087a8c6
hoss <agcacho22@gmail.com>
2021-04-15 22:26:00 -0700

    added file

---
+++ src/hooks/useAllowance.ts
@@ -0,0 +1,17 @@
+import { useWeb3React } from "@web3-react/core";
+import contracts from "config/constants/contracts";
+import { useCallback, useEffect } from "react";
+import { getAddress, getContractFromSymbol } from "utils/contractHelpers";
+
+const useAllowance = async () => {
+  const { account } = useWeb3React();
+  const pBallContract = getContractFromSymbol("pb2114");
+  const nftAddress = getAddress("pokemoonNft");
+
+  useEffect(() => {
+    const result = await pBallContract.methods
+      .allowance(account, nftAddress)
+      .call();
+  }, []);
+};
+export default useAllowance;
