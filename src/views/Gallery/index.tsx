bea78a4e6ef6a34319e49dc3ad11ccb54620e001
hoss <agcacho22@gmail.com>
2021-04-15 23:20:24 -0700

    fix carousel

--- src/views/Gallery/index.tsx
+++ src/views/Gallery/index.tsx
@@ -1,13 +1,12 @@
 import { Page, Content, Header } from "components/layout";
-import NFT_LIST from "config/constants/nfts/2114";
-import { Carousel } from "nft-uikit";
+import { Carousel, Blastoff } from "nft-uikit";
 
 const Gallery = () => {
   return (
     <>
       <Page>
         <Content>
-          <Carousel nfts={NFT_LIST}></Carousel>
+          <Carousel nfts={Blastoff.NFT_LIST}></Carousel>
         </Content>
       </Page>
     </>
