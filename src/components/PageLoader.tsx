28b626d071d9c2238d993aef57065a15d8ab929c
CJ <wynautcommunitywebdev@gmail.com>
2021-04-19 17:36:14 -0500

    🙃🙃

--- src/components/PageLoader.tsx
+++ src/components/PageLoader.tsx
@@ -12,7 +12,7 @@ const Wrapper = styled(Page)`
 const PageLoader: React.FC = () => {
   return (
     <Wrapper>
-      <WiggleBall />
+      <WiggleBall src="/images/balls/MAXRBALL.png" />
     </Wrapper>
   );
 };
