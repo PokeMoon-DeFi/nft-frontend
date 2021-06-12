ef38ea811555d6073a2321552633741090292a9d
Adrian <acacho@vertebrae.com>
2021-06-11 22:39:51 -0700

    get burn percent

--- src/providers/state/Market.ts
+++ src/providers/state/Market.ts
@@ -7,22 +7,48 @@ import { call, toNumber } from "utils/callHelpers";
 
 const marketplace = contracts.marketplace[56];
 
-const initialState = {};
+interface Listing {
+  id: number;
+  price: number;
+}
+
+interface MarketState {
+  listings: Listing[];
+  burnPercent: number;
+}
+
+const initialState: MarketState = {
+  listings: [],
+  burnPercent: 0,
+};
 
 export const fetchListings = createAsyncThunk(
   "market/fetchListings",
   async () => {
     const contract = new Contract(marketplace, marketAbi);
-    const calls = [contract.TFTLength(), contract.getTFT()];
+    const calls = [
+      contract.TFTLength(),
+      contract["dbe431f9"](),
+      contract.getTFT(),
+    ];
     const response = await call(calls);
-    const [listingsCount, listingInfo] = response;
+    let [listingsCount, burnPercent, listingInfo] = response;
+
+    burnPercent = toNumber(burnPercent) / 10000;
+
+    const listings: any = [];
     const [ids, prices] = listingInfo;
 
     for (let i = 0; i < listingsCount; i++) {
       const id = toNumber(ids[i]);
       const price = toNumber(prices[i]);
+      listings.push({
+        id,
+        price,
+      });
       console.log({ id, price });
     }
+    return { listings, burnPercent };
   }
 );
 
@@ -31,7 +57,10 @@ const marketSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchListings.fulfilled, (state, { payload }) => {});
+    builder.addCase(fetchListings.fulfilled, (state, { payload }) => {
+      state.burnPercent = payload.burnPercent;
+      state.listings = payload.listings;
+    });
   },
 });
 
